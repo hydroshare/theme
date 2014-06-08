b6759c09ce1c9ceba19dda703fb2818408c79ec5
Jeff Heard <jeff@renci.org>
2014-06-08 18:51:10 -0400

    fixed ugly CSS error with dropdown menus

--- templatetags/theme_tags.py
+++ templatetags/theme_tags.py
@@ -1,7 +1,6 @@
-
+from __future__ import unicode_literals
 from mezzanine import template
 from mezzanine.utils.sites import current_site_id
-
 from theme.models import SiteConfiguration
 
 register = template.Library()
@@ -13,3 +12,4 @@ def get_site_conf():
     Adds the `SiteConfiguration` to the context
     """
     return SiteConfiguration.objects.get_or_create(site_id=current_site_id())[0]
+
